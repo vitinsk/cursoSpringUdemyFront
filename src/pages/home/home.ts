820cb73c62f1d8f45909f4cb7e08085581d64212
Vitor Hugo <vitinsk@hotmail.com>
2018-08-28 21:25:09 -0300

    aproveitando usuario logado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,11 +26,19 @@ export class HomePage {
   ionViewDidLeave() {
   this.menu.swipeEnable(true);
   }
+  
+  ionViewDidEnter(){
+    this.auth.refreshToken()
+    .subscribe(response =>{     
+      this.auth.successfulLogin(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');
+    },
+    error => {});
+  }
 
   login(){
     this.auth.authenticate(this.creds)
-    .subscribe(response =>{
-      console.log(response.headers.get('Authorization'));
+    .subscribe(response =>{ 
       this.auth.successfulLogin(response.headers.get('Authorization'));
       this.navCtrl.setRoot('CategoriasPage');
     },
